a661474f814f8e57aad279549414b4a5415f21ca
Luis Daniel AH <luisdaniel.a.h.0307@outlook.com>
2021-11-08 10:23:07 -0600

    primer commit: crear formulario de login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,27 +3,6 @@ import 'package:flutter/material.dart';
 class PageLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            color: Colors.white,
-            margin: EdgeInsets.only(top: 20),
-            child: SizedBox(
-              width: 400,
-              height: 700,
-              child: Icon(
-                Icons.perm_identity,
-                size: 100,
-                color: Colors.black,
-              ),
-            ),
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-    );
+    return Container(child: Text('holaxd'));
   }
 }
